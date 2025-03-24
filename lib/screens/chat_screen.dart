d36f895ee1c7c5d5c216c2d7a9d5c2cf5d442507
fkscmc <fkscgw@outlook.com>
2025-03-25 05:31:11 +0800

    Update chat_screen.dart

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -1,1 +1,15 @@
+import 'package:flutter/material.dart';
 
+class ChatScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('聊天'),
+      ),
+      body: Center(
+        child: Text('这是聊天页面'),
+      ),
+    );
+  }
+}
