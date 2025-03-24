dbf1468a82a295325bf1ac5ae69ef9890c5218af
fkscmc <fkscgw@outlook.com>
2025-03-25 05:31:50 +0800

    Update group_chat_screen.dart

--- lib/screens/group_chat_screen.dart
+++ lib/screens/group_chat_screen.dart
@@ -1,1 +1,15 @@
+import 'package:flutter/material.dart';
 
+class GroupChatScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('群聊'),
+      ),
+      body: Center(
+        child: Text('这是群聊页面'),
+      ),
+    );
+  }
+}
