492caedf092b0318c55155b558ed6f7f48ce32ed
fkscmc <fkscgw@outlook.com>
2025-03-25 04:20:44 +0800

    Update auth_service.dart

--- lib/services/auth_service.dart
+++ lib/services/auth_service.dart
@@ -1,1 +1,23 @@
+import 'package:firebase_auth/firebase_auth.dart';
 
+class AuthService {
+  final FirebaseAuth _firebaseAuth = FirebaseAuth.instance;
+
+  Future<User?> signInAnonymously() async {
+    try {
+      UserCredential userCredential = await _firebaseAuth.signInAnonymously();
+      return userCredential.user;
+    } catch (e) {
+      print(e.toString());
+      return null;
+    }
+  }
+
+  Future<void> signOut() async {
+    await _firebaseAuth.signOut();
+  }
+
+  Stream<User?> get user {
+    return _firebaseAuth.authStateChanges();
+  }
+}
