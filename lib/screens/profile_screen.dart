71143ae18f9e6cc6915932347efd6841c7c1298c
fkscmc <fkscgw@outlook.com>
2025-03-25 05:32:27 +0800

    Update profile_screen.dart

--- lib/screens/profile_screen.dart
+++ lib/screens/profile_screen.dart
@@ -1,1 +1,15 @@
+import 'package:flutter/material.dart';
 
+class ProfileScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('用户资料'),
+      ),
+      body: Center(
+        child: Text('这是用户资料页面'),
+      ),
+    );
+  }
+}
